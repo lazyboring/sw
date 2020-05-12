c5a8b0a7573420aefc51f5bbafcfe56920d09d66
nc <1007749243@qq.com>
2020-05-12 17:11:50 +0800

    fdf

---
+++ nc-sw-web-api/src/main/java/com/nc/mrbs/web/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.nc.mrbs.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : niec
+ * @description
+ * @date : 2020-05-12 09:21
+ */
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+    @GetMapping("/ou")
+    public String get(){
+        return "cms/cms";
+    }
+}
